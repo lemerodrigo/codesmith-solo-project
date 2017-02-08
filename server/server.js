43ed91beace36689314fb3e22963247e2af63d6c
Rodrigo Leme <lemerodrigo@gmail.com>
2017-02-08 12:08:48 -0800

    Socket io online users working

--- server/server.js
+++ server/server.js
@@ -1,23 +1,49 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const path = require('path');
-// const bodyParser = require('body-parser');
+const forwarded = require('forwarded-for');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-// app.use(bodyParser.json());
-
-// /Users/rodrigo/Data/DEV/CODESMITH/codesmith-solo-project/server
-// app.use(express.static(path.join(__dirname, './../app')));
-// app.use(express.static(path.join(__dirname, './../build')));
 app.use(express.static(path.join(__dirname, './../')));
 
-// app.get('/messages', messageController.getMessages);
-// app.post('/messages', authController, messageController.postMessage);
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../index.html'));
+io.on('connection', () => {
+    console.log('A user connected!');
 });
 
+io.total = 0;
+io.users = [];
 
-app.listen(8080);
+io.on('connection', (socket) => {
+    const req = socket.request;
+    const ip = forwarded(req, req.headers);
+
+    io.total += 1;
+
+    socket.on('disconnect', () => {
+        io.total -= 1;
+        io.users = io.users.filter((item) => {
+            return item !== socket.name;
+        });
+        console.log('Disc', socket.name, io.users);
+        io.emit('onlineUsers', io.total, io.users);
+    });
+
+    // socket.broadcast.emit('onlineUsers', 'online users message');
+
+    socket.on('join', (data) => {
+        // if (socket.name) return;
+        socket.name = data.name;
+        // To client.
+        socket.emit('joined');
+        io.users.push(data.name);
+        io.emit('onlineUsers', io.total, io.users);
+    });
+
+});
+
+server.listen(8080, () => {
+    console.log('Listening on port 8080');
+});
 
 module.exports = app;
